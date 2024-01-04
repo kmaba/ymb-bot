b71784b28450686ffaa76bafc1c48ea06efbcae1
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2024-01-04 21:17:09 +0800

    Refactor webhook command to improve readability and error handling

--- commands/infos/webhook.js
+++ commands/infos/webhook.js
@@ -1,37 +1,31 @@
+const Discord = require('discord.js');
+
 module.exports = {
-    name: 'webhook',
-    aliases: 'wh',
-    utilisation: '{prefix}wh <user-id> <message>',
+    name: 'createWebhook',
+    aliases: ['wh', 'webhook'],
+	utilisation: '{prefix}wh <userid> <message>',
     execute(message, args) {
-        // Check if a user ID is provided
+        // Check if the user ID is provided
         if (!args[0]) {
-            return message.reply('You need to provide a user ID!');
+            return message.channel.send('You need to provide a user ID!');
         }
 
-        // Get the user ID
-        const userID = args[0];
-
         // Fetch the user
-        message.client.users.fetch(userID)
-            .then(user => {
-                // Get the second argument
-                const webhookMessage = args[1];
-
-                // Create a webhook
-                message.channel.createWebhook(user.username, {
-                    avatar: user.displayAvatarURL(),
-                })
-                .then(webhook => {
-                    // If a second argument is given, send a message to the webhook
-                    if (webhookMessage) {
-                        webhook.send(webhookMessage).then(() => {
-                            // Delete the webhook after 3 seconds
-                            setTimeout(() => webhook.delete(), 3000);
-                        });
-                    }
-                })
-                .catch(console.error);
+        message.client.users.fetch(args[0]).then(user => {
+            // Create a webhook with the user's name and avatar
+            message.channel.createWebhook(user.username, {
+                avatar: user.displayAvatarURL(),
             })
-            .catch(() => message.reply('Invalid user ID!'));
+            .then(webhook => {
+                // If a second argument is provided, send a message to the webhook
+                if (args[1]) {
+                    webhook.send(args[1]);
+                }
+            })
+            .catch(console.error);
+        })
+        .catch(() => {
+            message.reply('An error occurred while fetching the user!');
+        });
     },
 };
